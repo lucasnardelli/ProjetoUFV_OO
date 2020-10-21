7aaff91ea4264775858e5cfc0b24bb42ec874259
Daniel <Danielpereira227@gmai.com>
2020-10-21 14:02:04 -0300

    tabuleiro falta coluna

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -9,20 +9,35 @@ public class Tabuleiro {
 
     
     int tabuleiro1[][]=new int [5][5];
+
     public void mostrar() {
         for(int l=0;l<5;l++){
             for(int c=0;c<5;c++){
-                tabuleiro1[l][c]=0;
-                if( tabuleiro1[l][c]==0){
-                    System.out.print("____\n");
-                    System.out.print("|      |\n|      |" +
-                            "\n ____");
+                tabuleiro1[l][c]=1;
+
+            }
+        }
+        for(int l=0;l<5;l++){
+            for(int c=0;c<5;c++){
+                if(tabuleiro1[l][c]==1){
+
+                    System.out.print("_____");
+                    System.out.println();
+                    System.out.print("|   |");
+                    System.out.println();
+                    System.out.println("|   |");
+                    System.out.print("-----");
+                    System.out.println();
+                    System.out.print("\n");
+
 
                 }
-            }
-            System.out.println();
 
-        }        
+            }
+
+            System.out.print("\t");
+            System.out.print("\n");
+        }
     }
 }
 
