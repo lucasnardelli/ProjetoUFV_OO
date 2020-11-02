7a762c2c6aa8fcb6f57d73736244bad757482806
Daniel <Danielpereira227@gmai.com>
2020-11-02 15:07:20 -0300

    30%

--- TststeTabuleiro.java
+++ TststeTabuleiro.java
@@ -8,24 +8,15 @@ public class TststeTabuleiro {
         int local_linha;
         int local_coluna;
         int coluna_cont = 2;
-        
+        mat_1();
         do {
             mat[2][2] = 99;
             local_linha = gerador.nextInt(4);
             local_coluna = gerador.nextInt(4);
             mat[local_linha][local_coluna] = 80;
-            for (int i = 0; i < 5; i++) {
-                for (int c = 0; c < 5; c++) {
-                    if (i == 0 || c == 0 || i == 4 || c == 4) {
-                        // mat[i][c]=mat[i][c]*-1;//logica real
-                        // mat[i][c]=mat[i][c]=-1;
-                    }
-                    System.out.print("\t" + mat[i][c]);
-                }
-                System.out.println();
-            }
-        } while (mat[2][2] == 80);
 
+        } while (mat[2][2] == 80);
+//caminho do virus
         do {
             if (local_coluna < 2) {
                 mat[local_linha][coluna_cont] = 4;
@@ -37,5 +28,36 @@ public class TststeTabuleiro {
             }
         } while (coluna_cont != local_coluna);
 
+        extremidade();
+        mostrarTabuleiro();
     }
+
+    public void mat_1() {
+        for (int i = 0; i < 5; i++) {
+            for (int c = 0; c < 5; c++) {
+                mat[i][c] = 1;
+            }
+        }
+    }
+
+    public void extremidade() {
+        for (int i = 0; i < 5; i++) {
+            for (int c = 0; c < 5; c++) {
+                if (i == 0 || c == 0 || i == 4 || c == 4) {
+
+                    mat[i][c] = mat[i][c] * -1;//logica real
+                }
+            }
+        }
+    }
+
+    public void mostrarTabuleiro() {
+        for (int i = 0; i < 5; i++) {
+            for (int c = 0; c < 5; c++) {
+                System.out.print("\t" + mat[i][c]);
+            }
+            System.out.println();
+        }
+    }
+
 }
